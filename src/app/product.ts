eee7eb7a199985f51fe14c3874fe3aa0bfb65df0
omaryehia20002018 <142019609+omaryehia20002018@users.noreply.github.com>
2024-08-17 01:28:20 +0300

    added

---
+++ src/app/product.ts
@@ -0,0 +1,18 @@
+export interface Product {
+  id:number,
+  title:string,
+  price:string,
+  category:string,
+  description:string,
+  image:string
+
+  /*id: number;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+  stock_quantity: number;
+  store_id: number;
+  merchant_card_number: number;
+  merchantcvv: number;*/
+}
